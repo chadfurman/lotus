54cc62ceec30490fb72d7e6d73792c11fa131756
lotus <lotus@wtfayla.net>
2017-07-22 23:39:07 -0400

    gave env vars default values

--- config/globals.sh
+++ config/globals.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-VERSION="0.0.1"
-DATA="$DIR/data"
-LIB="$DIR/lib"
-BASE_INDENT=2
-HEADER_HEIGHT=3
-PROMPT_LINE=20
+DIR=${DIR:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"}
+CONFIG=${CONFIG:-"$DIR/config"}
+VERSION=${VERSION:-"0.0.1"}
+DATA=${DATA:-"$DIR/data"}
+SAVED_DATA=${SAVED_DATA:-"$DIR/saved_data"}
+LIB=${LIB:-"$DIR/lib"}
+BASE_INDENT=${BASE_INDENT:-2}
+HEADER_HEIGHT=${HEADER_HEIGHT:-3}
+PROMPT_LINE=${PROMPT_LINE:-20}
+
+if [ ! -d $DATA ]; then
+	mkdir $DATA
+fi
+if [ ! -d $SAVED_DATA ]; then
+	mkdir $SAVED_DATA
+fi
