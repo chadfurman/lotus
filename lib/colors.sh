95aa3c263d1fe551c74e2b98324164e6b70e29bf
lotus <lotus@wtfayla.net>
2017-07-22 23:40:19 -0400

    made colors print better and work stand-alone

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-colors=$(for color in $(seq 0 256); do
-	tput setaf $color
-	echo $color
-done;)
-echo $colors | xargs -n 30
+#!/usr/bin/env bash
+colors() {
+	for color in {0..256}; do
+		tput setaf $color
+		printf "%4d" "$color"
+		if [[ $(expr $color % 24) = 0 ]]; then
+			echo ''
+		fi
+	done;
+	echo ""
+	echo "ANSII Colors 0-256"
+}
+
+if [[ $_ == $0 ]]; then 
+	colors "$@"
+fi
